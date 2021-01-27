4f4998f9d1c992ac2eafd8c6739fb9e0e2e7c030
hero2 <hero2@DESKTOP-QPMTEVA>
2021-01-27 17:57:20 +0900

    캘린더 반복문 추가

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -6,25 +6,34 @@ public class Calendar {
 	
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	public int maxDaysofMonth(int month) {
+	public int getMaxDaysofMonth(int month) {
 		return MAX_DAYS[month - 1];
 	}
-
-	public static void main(String[] args) {
+	
+	public void printSampleCalendar() {
 		System.out.println("일	월	화	수	목	금	토");
 		System.out.println("--------------------------------------------------");
 		System.out.println(" 1	 2	 3	 4	 5	 6	 7");
 		System.out.println(" 8	 9	10	11	12	13	14");
 		System.out.println("15	16	17	18	19	20	21");
 		System.out.println("22	23	24	25	26	27	28");
+	}
 
+	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		Calendar cal = new Calendar();
 		
-		System.out.print("달을 입력하세요: ");
-		int month = sc.nextInt();
+		System.out.print("반복횟수를 입력하세요: ");
+		int cnt=sc.nextInt();
 		
-		System.out.printf("%d월은 %d일까지 있습니다.\n",month,cal.maxDaysofMonth(month));
+		for(int i=0;i<cnt;i++) {
+			System.out.print("달을 입력하세요: ");
+			int month = sc.nextInt();
+			
+			System.out.printf("%d월은 %d일까지 있습니다.\n",month,cal.getMaxDaysofMonth(month));
+		}
+		System.out.print("끝");
 		sc.close();
 	}
 
